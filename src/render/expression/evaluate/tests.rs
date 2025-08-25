bf9673ce99d4c5faa7a09e76a8a1e2aa356c8448
Mengxiao Lin <linmx0130@gmail.com>
2025-08-24 19:11:45 -0700

    Move expression-evaluation tests to own file
    - Split the former single module `evaluate.rs` into `evaluate/mod.rs` and `evaluate/tests.rs`.
    - Moved all unit tests from the bottom of `evaluate.rs` to the new `tests.rs` submodule.

---
+++ src/render/expression/evaluate/tests.rs
@@ -0,0 +1,228 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+use super::*;
+use serde_json::json;
+
+#[test]
+fn test_evaluate_reference() {
+  let Value::Object(variables) = json!({
+      "my": {"home":"127.0.0.1"},
+      "count": 5
+  }) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  assert_eq!(
+    evaluate_reference("my.home".as_bytes(), &context).unwrap(),
+    json!("127.0.0.1")
+  );
+  assert_eq!(
+    evaluate_reference("count".as_bytes(), &context).unwrap(),
+    json!(5)
+  );
+  assert_eq!(
+    evaluate_reference("my.car".as_bytes(), &context).unwrap(),
+    Value::Null
+  );
+  assert!(evaluate_reference("my.car.window".as_bytes(), &context).is_err());
+}
+
+#[test]
+fn test_evaluate_numbers() {
+  assert_eq!(evaluate_number("123".as_bytes()).unwrap(), json!(123));
+  assert_eq!(evaluate_number("0.55".as_bytes()).unwrap(), json!(0.55));
+}
+
+#[test]
+fn test_evaluate_array() {
+  let Value::Object(variables) = json!({}) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  assert_eq!(
+    evaluate_expression_tokens(
+      &[
+        ExpressionToken::LeftBracket,
+        ExpressionToken::Number(b"1"),
+        ExpressionToken::Comma,
+        ExpressionToken::String(b"\'2\'"),
+        ExpressionToken::Comma,
+        ExpressionToken::LeftBracket,
+        ExpressionToken::Ref(b"true"),
+        ExpressionToken::Comma,
+        ExpressionToken::Ref(b"false"),
+        ExpressionToken::Comma,
+        ExpressionToken::RightBracket,
+        ExpressionToken::Comma,
+        ExpressionToken::RightBracket,
+      ],
+      &context
+    )
+    .unwrap(),
+    json!([1, "2", [true, false]])
+  );
+}
+
+#[test]
+fn test_evaluate_not() {
+  let Value::Object(variables) = json!({"flag": false}) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  assert_eq!(
+    evaluate_expression_tokens(
+      &[ExpressionToken::Exclamation, ExpressionToken::Ref(b"flag"),],
+      &context
+    )
+    .unwrap(),
+    json!(true)
+  );
+}
+
+#[test]
+fn test_evaluate_plus_arith() {
+  let Value::Object(variables) = json!({
+      "a": 1,
+      "b": 2,
+      "s": "a"
+  }) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::Ref(b"b"),
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(3));
+  assert_eq!(pos, 3);
+
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::Ref(b"s"),
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!("1a"));
+  assert_eq!(pos, 3);
+}
+
+#[test]
+fn test_evaluate_minus_arith() {
+  let Value::Object(variables) = json!({
+      "a": 1,
+      "b": 2,
+      "c": 3.0,
+  }) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"-"),
+      ExpressionToken::Ref(b"b"),
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(-1));
+  assert_eq!(pos, 3);
+
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"-"),
+      ExpressionToken::Ref(b"c"),
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(-2.0));
+  assert_eq!(pos, 3);
+}
+
+#[test]
+fn test_evaluate_parenthesis() {
+  let Value::Object(variables) = json!({
+      "a": 1,
+      "b": 2,
+      "s": " "
+  }) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"-"),
+      ExpressionToken::LeftParenthesis,
+      ExpressionToken::Ref(b"b"),
+      ExpressionToken::ArithOp(b"-"),
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::RightParenthesis,
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(0));
+  assert_eq!(pos, 7);
+}
+
+#[test]
+fn test_mix_int_string_plus() {
+  let Value::Object(variables) = json!({
+      "a": 1,
+      "b": 2,
+      "s": " "
+  }) else {
+    panic!();
+  };
+  let context = RenderContext::from(variables);
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"s"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::LeftParenthesis,
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::Ref(b"b"),
+      ExpressionToken::RightParenthesis,
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(" 3"));
+  assert_eq!(pos, 7);
+
+  let (result, pos) = evaluate_expression_value(
+    &[
+      ExpressionToken::Ref(b"s"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::Ref(b"a"),
+      ExpressionToken::ArithOp(b"+"),
+      ExpressionToken::Ref(b"b"),
+    ],
+    0,
+    &context,
+  )
+  .unwrap();
+  assert_eq!(result, json!(" 12"));
+  assert_eq!(pos, 5);
+}
